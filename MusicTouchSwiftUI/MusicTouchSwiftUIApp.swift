464dd697295a7d5a659d59f77c5f7ee884206301
Pedro L. Diaz Montilla <pedroluis.diaz@gmail.com>
2021-08-11 18:43:50 +0200

    Initial Commit

---
+++ MusicTouchSwiftUI/MusicTouchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicTouchSwiftUIApp.swift
+//  MusicTouchSwiftUI
+//
+//  Created by Pedro L. Diaz Montilla on 11/8/21.
+//
+
+import SwiftUI
+
+@main
+struct MusicTouchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
